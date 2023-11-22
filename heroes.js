41e6f0d6134d0e0fde3e3de447c43bb665b5606c
NikaN <NikaN7602@gmail.com>
2023-11-22 19:23:54 +0100

    add test jest and files for tests

---
+++ heroes.js
@@ -0,0 +1,7 @@
+// heroes.js
+
+function sortHeroesByHealth(heroes) {
+    return heroes.slice().sort((a, b) => b.health - a.health);
+  }
+  
+  module.exports = { sortHeroesByHealth };
---
+++ heroes.test.js
@@ -0,0 +1,35 @@
+// heroes.test.js
+
+const { sortHeroesByHealth } = require('./heroes');
+
+test('sortHeroesByHealth sorts heroes by health in descending order', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sortedHeroes = sortHeroesByHealth(heroes);
+
+  expect(sortedHeroes).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
+
+test('sortHeroesByHealth does not modify the original array', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  sortHeroesByHealth(heroes);
+
+  expect(heroes).toEqual([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+});
